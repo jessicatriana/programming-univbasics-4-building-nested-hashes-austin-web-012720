1259c74527f72e15aa4388dc0f532cf2cd0e23e6
Jessica Triana <jessica.triana.tx@gmail.com>
2019-12-19 21:06:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,7 +12,7 @@ def monopoly_with_third_tier
   monopoly = {
     :railroads => {
       :names => {:key1 => [], :key2 => [], :key3 => [], :key4 => [] },
-      :rent_in_dollars => {:key1 => [], :key2 => [], :key3 => [], :key4 => [] }
+      :rent_in_dollars => {:one_piece_owned => 25, :key2 => [], :key3 => [], :key4 => [] }
 }
   }
   monopoly
